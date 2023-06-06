46141a3681b759032c352877b861ea5d3fd8a727
Olivier Cartier <me@cestoliv.com>
2023-06-06 08:00:21 +0000

    style: :sparkles: cleaning code

--- 1-array/ex03/zoom.py
+++ 1-array/ex03/zoom.py
@@ -1,34 +1,42 @@
 from load_image import ft_load
-from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = ft_load('animal.jpeg')
 
 def trim(array, x, y, width, height, depth=3):
-	return array[y:y+width, x:x+height, :depth]
-
-print('The shape of image is', image.shape)
-print(image)
-
-im_trim = trim(image, 450, 100, 400, 400, 1)
-# im_trim = image[:400, :400, :1]
-
-# im_trim = Image.fromarray(im_trim).convert('L')
-# im_trim = np.asarray(im_trim)
-# im_trim = np.dot(im_trim[...,:3], [0.2989, 0.5870, 0.1140])
-
-print("After trimming:",im_trim.shape, 'or', im_trim.shape[0], 'x', im_trim.shape[1])
-
-print(im_trim)
-
-# Image.fromarray(im_trim).show()
+    """
+        Trim an array using the given parameters
+    """
+    return array[y:y+width, x:x+height, :depth]
 
 
-sliced_image = np.squeeze(im_trim)
-# print(sliced_image)
+def main():
+    """
+        Open the image, trim it and convert it to grayscale,
+        then display it.
+    """
 
-# Display the image
-plt.imshow(sliced_image, cmap='gray')
-plt.axis('off')  # Remove the axes
-plt.show()
+    try:
+        image = ft_load('animal.jpeg')
+    except Exception as e:
+        print(e)
+        exit()
+
+    print('The shape of image is', image.shape)
+    print(image)
+
+    image = trim(image, 450, 100, 400, 400, 1)
+
+    print(f'New shape after slicing: {image.shape}', end='')
+    print(f' or ({image.shape[0]}, {image.shape[1]})')
+    print(image)
+
+    # Display the image
+    image = np.squeeze(image)
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
