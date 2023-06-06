c9459a38e38cf05f66c2524e54271753e7d6a168
Olivier Cartier <me@cestoliv.com>
2023-06-06 09:29:41 +0000

    fix: :bug: correct formatting

--- 0-starting/ex01/format_ft_time.py
+++ 0-starting/ex01/format_ft_time.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 date = datetime.today()
-print(f'Seconds since January 1, 1970: {date.timestamp():,}', end='')
-print(f'or {f"{date.timestamp():.2E}".lower()} in scientific notation')
+print(f'Seconds since January 1, 1970: {date.timestamp():,.4f}', end='')
+print(f' or {f"{date.timestamp():.2E}".lower()} in scientific notation')
 print(date.strftime('%b %d %Y'))
