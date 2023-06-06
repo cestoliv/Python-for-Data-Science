c392349ee78491472f36640a78b4d424a4f3706b
Olivier Cartier <me@cestoliv.com>
2023-06-06 10:40:59 +0000

    fix: :bug: fix ft_type

--- 0-starting/ex02/find_ft_type.py
+++ 0-starting/ex02/find_ft_type.py
@@ -8,7 +8,7 @@ def all_thing_is_obj(object: any) -> int:
     elif type(object) == dict:
         print('Dict :', type(object))
     elif type(object) == str:
-        print(object, ':', type(object))
+        print(object, 'is in the kitchen :', type(object))
     else:
         print('Type not found')
     return 42
