ba58f9c11f16fa8fd09a98c018c29fd6ff15b5b3
Olivier Cartier <me@cestoliv.com>
2023-06-06 10:45:02 +0000

    feat: :sparkles: ex05 ok

--- 1-array/ex05/pimp_image.py
+++ 1-array/ex05/pimp_image.py
@@ -1,14 +1,21 @@
-from load_image import ft_load
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def ft_invert(array) -> np.ndarray:
+    """
+        Take an image array, and return a new array
+        with colors inverted
+    """
     invTransform = np.vectorize(lambda x: 255 - x)
     invert = invTransform(array)
     return invert
 
+
 def ft_red(array) -> np.ndarray:
+    """
+        Take an image array, and return a new array
+        with a red color filter
+    """
     red = array.copy()
     for i in range(len(red)):
         for j in range(len(red[i])):
@@ -17,7 +24,12 @@ def ft_red(array) -> np.ndarray:
 
     return red
 
+
 def ft_green(array) -> np.ndarray:
+    """
+        Take an image array, and return a new array
+        with a green color filter
+    """
     green = array.copy()
     for i in range(len(green)):
         for j in range(len(green[i])):
@@ -26,7 +38,12 @@ def ft_green(array) -> np.ndarray:
 
     return green
 
+
 def ft_blue(array) -> np.ndarray:
+    """
+        Take an image array, and return a new array
+        with a blue color filter
+    """
     blue = array.copy()
     for i in range(len(blue)):
         for j in range(len(blue[i])):
@@ -35,11 +52,15 @@ def ft_blue(array) -> np.ndarray:
 
     return blue
 
+
 def ft_grey(array) -> np.ndarray:
+    """
+        Take an image array, and return a new array
+        with a grayscale
+    """
     grey = array.copy()
     for i in range(len(grey)):
         for j in range(len(grey[i])):
-            # val = 0.2989 * grey[i][j][2] + 0.5870 * grey[i][j][1] + 0.1140 * grey[i][j][0]
             val = 0
             if (grey[i][j][2] > 0):
                 val += 0.2989 / (1 / grey[i][j][2])
@@ -49,34 +70,3 @@ def ft_grey(array) -> np.ndarray:
                 val += 0.1140 / (1 / grey[i][j][0])
             grey[i][j][0] = grey[i][j][1] = grey[i][j][2] = val
     return grey
-
-def display_image(array):
-    array = np.squeeze(array)
-    plt.imshow(array, cmap='gray')
-    plt.axis('off')
-    plt.show()
-
-def main():
-    """
-        Open the image and display it with differents color filters
-    """
-
-    try:
-        image = ft_load('landscape.jpg')
-    except Exception as e:
-        print(e)
-        exit()
-
-    print(f'The shape of the image is: {image.shape}')
-    print(image)
-
-    display_image(image)
-    display_image(ft_invert(image))
-    display_image(ft_red(image))
-    display_image(ft_green(image))
-    display_image(ft_blue(image))
-    display_image(ft_grey(image))
-
-
-if __name__ == "__main__":
-    main()
