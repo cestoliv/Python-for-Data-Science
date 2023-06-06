3e3753f475a62a23c66d4db2068b18b08543b0a4
Olivier Cartier <me@cestoliv.com>
2023-06-06 09:03:16 +0200

    feat: ✨ ex01 ok

--- 1-array/ex01/array2D.py
+++ 1-array/ex01/array2D.py
@@ -1,7 +1,11 @@
 import numpy as np
 
+
 def slice_me(family: list, start: int, end: int) -> list:
     if type(family) != list:
         raise ValueError('Family must be a list')
     family = np.asarray(family)
-    print(family.shape)
+    print("My shape is", family.shape)
+    family = family[start:end]
+    print("My new shape is", family.shape)
+    return family
