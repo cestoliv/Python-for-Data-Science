95600759e8b29e166e8f7c3fbc61115814fbdc58
Olivier Cartier <me@cestoliv.com>
2023-06-07 12:03:04 +0000

    docs: :sparkles: adding docstring

--- 3-oop/ex00/S1E9.py
+++ 3-oop/ex00/S1E9.py
@@ -2,7 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
+    """A Character with a first_name and a is_alive property"""
     def __init__(self, first_name: str, is_alive=True):
+        """Init a character"""
         if not isinstance(first_name, str):
             raise TypeError("first_name must be a string")
         if not isinstance(is_alive, bool):
@@ -12,12 +14,16 @@ class Character(ABC):
 
     @abstractmethod
     def die(self):
+        """Abstract die class"""
         self.is_alive = False
 
 
 class Stark(Character):
+    """A Stark that inherit from a Character"""
     def __init__(self, first_name: str, is_alive=True):
+        """Init a Stark"""
         super().__init__(first_name, is_alive)
 
     def die(self):
+        """The Stark die"""
         super().die()
